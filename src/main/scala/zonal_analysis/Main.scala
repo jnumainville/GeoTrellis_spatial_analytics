57ebaf173a35f1dea2d53c4e7d84fda896b51562
David Haynes <dahaynes@umn.edu>
2018-09-16 10:11:00 -0500

    Zonal function working on different raster datasets

--- src/main/scala/zonal_analysis/Main.scala
+++ src/main/scala/zonal_analysis/Main.scala
@@ -4,9 +4,11 @@ import geotrellis.raster._
 import geotrellis.spark._
 import geotrellis.vector.io._
 import geotrellis.vector.io.json._
+
 import scala.io.StdIn.{readInt, readLine}
 import geotrellis.raster.io.geotiff.reader.GeoTiffReader
 import geotrellis.raster.io.geotiff._
+
 import scala.io.Source
 import geotrellis.vector._
 import geotrellis.vector.io._
@@ -14,6 +16,7 @@ import spray.json._
 import spray.json.DefaultJsonProtocol._
 import geotrellis.raster._
 import datasets.rasterdatasets.myRaster
+import datasets.vectordatasets.myVector
 //Has TileLayout Object, MultibandTile
 import geotrellis.raster.io.geotiff._
 import geotrellis.raster.render._
@@ -47,6 +50,7 @@ import spray.json.DefaultJsonProtocol._
 //File Object
 import scala.io.StdIn
 import java.io.File
+import java.io._
 import scala.collection.mutable.ListBuffer
 import org.apache.log4j.{Level, Logger}
 
@@ -58,105 +62,104 @@ object Main {
     Logger.getLogger("org.apache.spark").setLevel(Level.ERROR)
     //Raster Dataset Path
     val rasterDatasets = List(
-      new myRaster("glc", "/home/david/Downloads/glc2000.tif", 16, 1, 4326)
+      new myRaster("glc", "/home/david/Downloads/glc2000.tif", 16, 1, 4326),
     //new myRaster("glc", "/data/projects/G-818404/glc2000_clipped.tif", 16, 1),
     //new myRaster("meris", "/data/projects/G-818404/meris_2010_clipped.tif", 100, 1),
+      new myRaster("nlcd", "/home/david/Downloads/nlcd_2006.tif", 21, 1, 5070)
     //new myRaster("nlcd", "/data/projects/G-818404/nlcd_2006.tif", 21, 1)
     //new rasterdataset("meris_3m", "/data/projects/G-818404/meris_2010_clipped_3m/", 100, 1)
     )
 
     val vectorDatasets = List(
-      ("states", "/home/david/shapefiles/4326/states_2.geojson")
+     new myVector("states", "/home/david/shapefiles", "states_2.geojson")
     )
 
     val tileSizes = Array(25, 50, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000) //, 1500, 2000, 2500, 3000, 3500, 4000)
 
     val outSummaryStats = "/home/david/geotrellis_glc_stats_zonalstats.csv"
     //val writer = new BufferedWriter(new )
+    val outCSVPath = "/home/david/output.csv"  //"/data/projects/G-818404/geotrellis_localcount_8_27_2018_12instances.csv" //
+    val writer = new PrintWriter(new File(outCSVPath))
+    writer.write("analytic,raster_dataset,tilesize,vector_dataset,total_time,multipolygon_time, polygon_time, run\n")
 
     val conf = new SparkConf().setMaster("local[2]").setAppName("Zonal Stats").set("spark.serializer", "org.apache.spark.serializer.KryoSerializer").set("spark.kryo.regisintrator", "geotrellis.spark.io.kryo.KryoRegistrator")
     implicit val sc = new SparkContext(conf)
 
     for(r<-rasterDatasets){
-//      for (tile <- tileSizes){
+//      for (tilesize <- tileSizes){
+        val tilesize = 250
 
         //val geoTiff: SinglebandGeoTiff = SinglebandGeoTiff(r.thePath)
         val rasterRDD: RDD[(ProjectedExtent, Tile)] = HadoopGeoTiffRDD.spatial(r.thePath, HadoopGeoTiffRDD.Options.DEFAULT)
-        val (_,rasterMetaData) = TileLayerMetadata.fromRdd(rasterRDD, FloatingLayoutScheme(250))
+        val (_,rasterMetaData) = TileLayerMetadata.fromRdd(rasterRDD, FloatingLayoutScheme(tilesize))
         val tiledRaster: RDD[(SpatialKey,geotrellis.raster.Tile)] = rasterRDD.tileToLayout(rasterMetaData.cellType, rasterMetaData.layout)
         val rasterTileLayerRDD: TileLayerRDD[SpatialKey] = ContextRDD(tiledRaster, rasterMetaData)
-        //number of partitions RDD.keys.toList.length
 
+        //Removing other RDD
         rasterRDD.unpersist()
         tiledRaster.unpersist()
-        // for(v<-vectorDatasets){}
-        // val file: String = "/home/david/shapefiles/4326/states_2.geojson" //"data/censusMetroNew.geojson"
-        val jsonPath  = vectorDatasets(0)._2
-        val theJSON = scala.io.Source.fromFile(jsonPath).getLines.mkString
-        case class Attributes(NAME: String,LSAD: String,AFFGEOID: String,ALAND: Int, AWATER: Int, ID: Int)
-        implicit val boxedToRead = jsonFormat6(Attributes)
+        //for (theRun <- 1 to 3){
+        val theRun  = 1
+        for(v<-vectorDatasets) {
+          // val file: String = "/home/david/shapefiles/4326/states_2.geojson" //"data/censusMetroNew.geojson"
+          var jsonPath  = v.theBasePath +"/" + r.srid + "/" + v.theJSON
+          //val jsonPath = vectorDatasets(0)._2
+          val theJSON = scala.io.Source.fromFile(jsonPath).getLines.mkString
+          case class Attributes(NAME: String, LSAD: String, AFFGEOID: String, ALAND: Int, AWATER: Int, ID: Int)
+          implicit val boxedToRead = jsonFormat6(Attributes)
 
-        // This needs to be 2 separate functions
-        val multiPolygons: Map[String, MultiPolygonFeature[Attributes]] = theJSON.parseGeoJson[JsonFeatureCollectionMap].getAllMultiPolygonFeatures[Attributes]
-        val polygons: Map[String, PolygonFeature[Attributes]] = theJSON.parseGeoJson[JsonFeatureCollectionMap].getAllPolygonFeatures[Attributes]
+          // GeoTrellis does not handle both multipolygons and polgyons in the same function
+          val multiPolygons: Map[String, MultiPolygonFeature[Attributes]] = theJSON.parseGeoJson[JsonFeatureCollectionMap].getAllMultiPolygonFeatures[Attributes]
+          val polygons: Map[String, PolygonFeature[Attributes]] = theJSON.parseGeoJson[JsonFeatureCollectionMap].getAllPolygonFeatures[Attributes]
 
+          //Potential object for writing outvalues
+          var ZonalStats = new ListBuffer[Map[String, (Int, Int, Double)]]()
 
-        //def MultiPolygonSummaryStats(mp: Map[String,geotrellis.vector.MultiPolygonFeature[Attributes]] :org.apache.spark.rdd.RDD[(geotrellis.spark.SpatialKey, geotrellis.raster.Tile)], oldValue:Int, newValue:Int)  = {
+          var zonalStatsStart = System.currentTimeMillis()
 
-        val theMultiPolygonKeys = multiPolygons.keys.toList
-        val thePolygonKeys = polygons.keys.toList
+          val multiGeom = multiPolygons.mapValues(x => x.geom)
+          val multiHistogram = multiGeom.mapValues(x => rasterTileLayerRDD.polygonalHistogram(x))
+          val multiPolyStats = multiHistogram.mapValues(x => x.statistics.toList)
+          for (k <- multiPolyStats.keys) {
+            println(k, multiPolyStats(k))
+          }
 
-        var ZonalStats = new ListBuffer[Map[String, (Int, Int, Double)]]()
+          /*        for (i<-0 to theMultiPolygonKeys.length-1){
 
-        var zonalStatsStart = System.currentTimeMillis()
+            //var geom = multiPolygons.get(theMultiPolygonKeys(0).toString).get.geom
+            var geom = multiPolygons.get(theMultiPolygonKeys(i).toString).get.geom
+            var histogram = rasterTileLayerRDD.polygonalHistogram(geom)
+            var theStats = histogram.statistics
+            //var theMean = histogram.mean.min
+            println(theMultiPolygonKeys(i).toString, theStats)
 
-        var geometries = multiPolygons.mapValues(x=> x.geom)
-        var histogram = geometries.mapValues(x => rasterTileLayerRDD.polygonalHistogram(x))
-        val multiPolyStats = histogram.mapValues(x => x.statistics.toList)  //count.min._1)
-        for( k <- multiPolyStats.keys) {
-          println(multiPolyStats(k))
-        }
+            //ZonalStats += Map(theMultiPolygonKeys(i).toString -> (theMin, theMax, theMean))
+            //Map("x" -> 24, "y" -> 25, "z" -> 26)
 
-/*        for (i<-0 to theMultiPolygonKeys.length-1){
-
-          //var geom = multiPolygons.get(theMultiPolygonKeys(0).toString).get.geom
-          var geom = multiPolygons.get(theMultiPolygonKeys(i).toString).get.geom
-          var histogram = rasterTileLayerRDD.polygonalHistogram(geom)
-          var theStats = histogram.statistics
-          //var theMean = histogram.mean.min
-          println(theMultiPolygonKeys(i).toString, theStats)
-
-          //ZonalStats += Map(theMultiPolygonKeys(i).toString -> (theMin, theMax, theMean))
-          //Map("x" -> 24, "y" -> 25, "z" -> 26)
-
-        }*/
+          }*/
 
 
-        var zonalStatsStop = System.currentTimeMillis()
-        var finalTime = zonalStatsStop-zonalStatsStart
-        println("Time to complete: ", finalTime)
-        println("*********** Finished multipolygons ***************")
+          var zonalStatsStop = System.currentTimeMillis()
+          var multiPolygonTime = zonalStatsStop - zonalStatsStart
+          println("Time to complete multipolygons: ", multiPolygonTime)
+          println("*********** Finished multipolygons ***************")
 
-        zonalStatsStart = System.currentTimeMillis()
-        geometries = polygons.mapValues(x=> x.geom)
-        histogram = geometries.mapValues(x => rasterTileLayerRDD.polygonalHistogram(x))
-        //val polygonMeans = histogram.mapValues(x => x.mean.min)
-        //val polyStats = histogram.mapValues(x => x.statistics)
-        //val (polyMin, polyMax) = histogram.mapValues(x => x.minMaxValues.min)
-        //val polyMin = histogram.mapValues(x => x.minMaxValues.min._1)
-        //val polyMax = histogram.mapValues(x => x.minMaxValues.min._2)
-        val polyStats = histogram.mapValues(x => x.statistics.toList)  //count.min._1)
-        for( k <- polyStats.keys) {
-          println(polyStats(k))
-        }
-        //val polyStats = polygonMeans.toList ++ polyMinMax.toList
-        //val polys = polyStats.groupBy(_._1).map{case(k, v) => k -> v.map(_._2).toSeq}
+          zonalStatsStart = System.currentTimeMillis()
+          val polyGeom = polygons.mapValues(x => x.geom)
+          val polyHistogram = polyGeom.mapValues(x => rasterTileLayerRDD.polygonalHistogram(x))
+          //val polygonMeans = histogram.mapValues(x => x.mean.min)
+          //val polyStats = histogram.mapValues(x => x.statistics)
+          //val (polyMin, polyMax) = histogram.mapValues(x => x.minMaxValues.min)
+          //val polyMin = histogram.mapValues(x => x.minMaxValues.min._1)
+          //val polyMax = histogram.mapValues(x => x.minMaxValues.min._2)
+          val polyStats = polyHistogram.mapValues(x => x.statistics.toList) //count.min._1)
+          for (k <- polyStats.keys) {
+            println(k, polyStats(k))
+          }
 
 
 
-
-
-/*        for (i<-0 to thePolygonKeys.length-1){
+          /*        for (i<-0 to thePolygonKeys.length-1){
 
           var geom = polygons.get(thePolygonKeys(i).toString).get.geom
           var histogram = rasterTileLayerRDD.polygonalHistogram(geom)
@@ -169,21 +172,23 @@ object Main {
 
         }*/
 
-        zonalStatsStop = System.currentTimeMillis()
-        finalTime = zonalStatsStop-zonalStatsStart
-        println("Time to complete: ", finalTime)
-/*        val multiPolygonStats = ZonalStats.toList
-        val thePolygonKeys = polygons.keys.toList*/
-        // ZonalStats.toList.foreach(writer.write)
+          zonalStatsStop = System.currentTimeMillis()
+          var polygonTime = zonalStatsStop - zonalStatsStart
+          println("Time to complete polygons: ", polygonTime)
+          var totalTime = polygonTime + multiPolygonTime
+          println("Total Time to complete: ", totalTime)
 
-      }
-
-      //      }
+          writer.write("polygonal_summary,$r.name,$tilesize,$v.name,$totalTime,$multiPolygonTime, $polygonTime, $theRun\n")
 
 
+          /*        val multiPolygonStats = ZonalStats.toList
+          val thePolygonKeys = polygons.keys.toList*/
+          // ZonalStats.toList.foreach(writer.write)
 
+        } //vector
+      //tile
+    }//raster
 
-//    }
 
 
     sc.stop()
