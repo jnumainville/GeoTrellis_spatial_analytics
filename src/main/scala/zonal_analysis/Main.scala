ba2be9d4345691a18539b210efe13c0077397507
David Haynes <dahaynes@umn.edu>
2018-09-16 10:15:18 -0500

    Zonal function with all loops

--- src/main/scala/zonal_analysis/Main.scala
+++ src/main/scala/zonal_analysis/Main.scala
@@ -85,109 +85,112 @@ object Main {
     val conf = new SparkConf().setMaster("local[2]").setAppName("Zonal Stats").set("spark.serializer", "org.apache.spark.serializer.KryoSerializer").set("spark.kryo.regisintrator", "geotrellis.spark.io.kryo.KryoRegistrator")
     implicit val sc = new SparkContext(conf)
 
-    for(r<-rasterDatasets){
-//      for (tilesize <- tileSizes){
-        val tilesize = 250
+    for(r<-rasterDatasets) {
+
+      for (tilesize <- tileSizes) {
+        //val tilesize = 250
 
         //val geoTiff: SinglebandGeoTiff = SinglebandGeoTiff(r.thePath)
         val rasterRDD: RDD[(ProjectedExtent, Tile)] = HadoopGeoTiffRDD.spatial(r.thePath, HadoopGeoTiffRDD.Options.DEFAULT)
-        val (_,rasterMetaData) = TileLayerMetadata.fromRdd(rasterRDD, FloatingLayoutScheme(tilesize))
-        val tiledRaster: RDD[(SpatialKey,geotrellis.raster.Tile)] = rasterRDD.tileToLayout(rasterMetaData.cellType, rasterMetaData.layout)
+        val (_, rasterMetaData) = TileLayerMetadata.fromRdd(rasterRDD, FloatingLayoutScheme(tilesize))
+        val tiledRaster: RDD[(SpatialKey, geotrellis.raster.Tile)] = rasterRDD.tileToLayout(rasterMetaData.cellType, rasterMetaData.layout)
         val rasterTileLayerRDD: TileLayerRDD[SpatialKey] = ContextRDD(tiledRaster, rasterMetaData)
 
         //Removing other RDD
         rasterRDD.unpersist()
         tiledRaster.unpersist()
-        //for (theRun <- 1 to 3){
-        val theRun  = 1
-        for(v<-vectorDatasets) {
-          // val file: String = "/home/david/shapefiles/4326/states_2.geojson" //"data/censusMetroNew.geojson"
-          var jsonPath  = v.theBasePath +"/" + r.srid + "/" + v.theJSON
-          //val jsonPath = vectorDatasets(0)._2
-          val theJSON = scala.io.Source.fromFile(jsonPath).getLines.mkString
-          case class Attributes(NAME: String, LSAD: String, AFFGEOID: String, ALAND: Int, AWATER: Int, ID: Int)
-          implicit val boxedToRead = jsonFormat6(Attributes)
+        for (theRun <- 1 to 3) {
+          //val theRun = 1
+          for (v <- vectorDatasets) {
+            // val file: String = "/home/david/shapefiles/4326/states_2.geojson" //"data/censusMetroNew.geojson"
+            var jsonPath = v.theBasePath + "/" + r.srid + "/" + v.theJSON
+            //val jsonPath = vectorDatasets(0)._2
+            val theJSON = scala.io.Source.fromFile(jsonPath).getLines.mkString
+            case class Attributes(NAME: String, LSAD: String, AFFGEOID: String, ALAND: Int, AWATER: Int, ID: Int)
+            implicit val boxedToRead = jsonFormat6(Attributes)
 
-          // GeoTrellis does not handle both multipolygons and polgyons in the same function
-          val multiPolygons: Map[String, MultiPolygonFeature[Attributes]] = theJSON.parseGeoJson[JsonFeatureCollectionMap].getAllMultiPolygonFeatures[Attributes]
-          val polygons: Map[String, PolygonFeature[Attributes]] = theJSON.parseGeoJson[JsonFeatureCollectionMap].getAllPolygonFeatures[Attributes]
+            // GeoTrellis does not handle both multipolygons and polgyons in the same function
+            val multiPolygons: Map[String, MultiPolygonFeature[Attributes]] = theJSON.parseGeoJson[JsonFeatureCollectionMap].getAllMultiPolygonFeatures[Attributes]
+            val polygons: Map[String, PolygonFeature[Attributes]] = theJSON.parseGeoJson[JsonFeatureCollectionMap].getAllPolygonFeatures[Attributes]
 
-          //Potential object for writing outvalues
-          var ZonalStats = new ListBuffer[Map[String, (Int, Int, Double)]]()
+            //Potential object for writing outvalues
+            var ZonalStats = new ListBuffer[Map[String, (Int, Int, Double)]]()
 
-          var zonalStatsStart = System.currentTimeMillis()
+            var zonalStatsStart = System.currentTimeMillis()
 
-          val multiGeom = multiPolygons.mapValues(x => x.geom)
-          val multiHistogram = multiGeom.mapValues(x => rasterTileLayerRDD.polygonalHistogram(x))
-          val multiPolyStats = multiHistogram.mapValues(x => x.statistics.toList)
-          for (k <- multiPolyStats.keys) {
-            println(k, multiPolyStats(k))
-          }
+            val multiGeom = multiPolygons.mapValues(x => x.geom)
+            val multiHistogram = multiGeom.mapValues(x => rasterTileLayerRDD.polygonalHistogram(x))
+            val multiPolyStats = multiHistogram.mapValues(x => x.statistics.toList)
+            for (k <- multiPolyStats.keys) {
+              println(k, multiPolyStats(k))
+            }
 
-          /*        for (i<-0 to theMultiPolygonKeys.length-1){
+            /*        for (i<-0 to theMultiPolygonKeys.length-1){
 
-            //var geom = multiPolygons.get(theMultiPolygonKeys(0).toString).get.geom
-            var geom = multiPolygons.get(theMultiPolygonKeys(i).toString).get.geom
+              //var geom = multiPolygons.get(theMultiPolygonKeys(0).toString).get.geom
+              var geom = multiPolygons.get(theMultiPolygonKeys(i).toString).get.geom
+              var histogram = rasterTileLayerRDD.polygonalHistogram(geom)
+              var theStats = histogram.statistics
+              //var theMean = histogram.mean.min
+              println(theMultiPolygonKeys(i).toString, theStats)
+
+              //ZonalStats += Map(theMultiPolygonKeys(i).toString -> (theMin, theMax, theMean))
+              //Map("x" -> 24, "y" -> 25, "z" -> 26)
+
+            }*/
+
+
+            var zonalStatsStop = System.currentTimeMillis()
+            var multiPolygonTime = zonalStatsStop - zonalStatsStart
+            println("Time to complete multipolygons: ", multiPolygonTime)
+            println("*********** Finished multipolygons ***************")
+
+            zonalStatsStart = System.currentTimeMillis()
+            val polyGeom = polygons.mapValues(x => x.geom)
+            val polyHistogram = polyGeom.mapValues(x => rasterTileLayerRDD.polygonalHistogram(x))
+            //val polygonMeans = histogram.mapValues(x => x.mean.min)
+            //val polyStats = histogram.mapValues(x => x.statistics)
+            //val (polyMin, polyMax) = histogram.mapValues(x => x.minMaxValues.min)
+            //val polyMin = histogram.mapValues(x => x.minMaxValues.min._1)
+            //val polyMax = histogram.mapValues(x => x.minMaxValues.min._2)
+            val polyStats = polyHistogram.mapValues(x => x.statistics.toList) //count.min._1)
+            for (k <- polyStats.keys) {
+              println(k, polyStats(k))
+            }
+
+
+
+            /*        for (i<-0 to thePolygonKeys.length-1){
+
+            var geom = polygons.get(thePolygonKeys(i).toString).get.geom
             var histogram = rasterTileLayerRDD.polygonalHistogram(geom)
-            var theStats = histogram.statistics
-            //var theMean = histogram.mean.min
-            println(theMultiPolygonKeys(i).toString, theStats)
+            var(theMin, theMax) = histogram.minMaxValues.min
+            var theMean = histogram.mean.min
+            println(thePolygonKeys(i).toString, theMin, theMax, theMean)
 
-            //ZonalStats += Map(theMultiPolygonKeys(i).toString -> (theMin, theMax, theMean))
-            //Map("x" -> 24, "y" -> 25, "z" -> 26)
+            ZonalStats += Map(thePolygonKeys(i).toString -> (theMin, theMax, theMean))
+
 
           }*/
 
+            zonalStatsStop = System.currentTimeMillis()
+            var polygonTime = zonalStatsStop - zonalStatsStart
+            println("Time to complete polygons: ", polygonTime)
+            var totalTime = polygonTime + multiPolygonTime
+            println("Total Time to complete: ", totalTime)
 
-          var zonalStatsStop = System.currentTimeMillis()
-          var multiPolygonTime = zonalStatsStop - zonalStatsStart
-          println("Time to complete multipolygons: ", multiPolygonTime)
-          println("*********** Finished multipolygons ***************")
-
-          zonalStatsStart = System.currentTimeMillis()
-          val polyGeom = polygons.mapValues(x => x.geom)
-          val polyHistogram = polyGeom.mapValues(x => rasterTileLayerRDD.polygonalHistogram(x))
-          //val polygonMeans = histogram.mapValues(x => x.mean.min)
-          //val polyStats = histogram.mapValues(x => x.statistics)
-          //val (polyMin, polyMax) = histogram.mapValues(x => x.minMaxValues.min)
-          //val polyMin = histogram.mapValues(x => x.minMaxValues.min._1)
-          //val polyMax = histogram.mapValues(x => x.minMaxValues.min._2)
-          val polyStats = polyHistogram.mapValues(x => x.statistics.toList) //count.min._1)
-          for (k <- polyStats.keys) {
-            println(k, polyStats(k))
-          }
+            writer.write("polygonal_summary,$r.name,$tilesize,$v.name,$totalTime,$multiPolygonTime, $polygonTime, $theRun\n")
 
 
-
-          /*        for (i<-0 to thePolygonKeys.length-1){
-
-          var geom = polygons.get(thePolygonKeys(i).toString).get.geom
-          var histogram = rasterTileLayerRDD.polygonalHistogram(geom)
-          var(theMin, theMax) = histogram.minMaxValues.min
-          var theMean = histogram.mean.min
-          println(thePolygonKeys(i).toString, theMin, theMax, theMean)
-
-          ZonalStats += Map(thePolygonKeys(i).toString -> (theMin, theMax, theMean))
-
-
-        }*/
-
-          zonalStatsStop = System.currentTimeMillis()
-          var polygonTime = zonalStatsStop - zonalStatsStart
-          println("Time to complete polygons: ", polygonTime)
-          var totalTime = polygonTime + multiPolygonTime
-          println("Total Time to complete: ", totalTime)
-
-          writer.write("polygonal_summary,$r.name,$tilesize,$v.name,$totalTime,$multiPolygonTime, $polygonTime, $theRun\n")
-
-
-          /*        val multiPolygonStats = ZonalStats.toList
+            /*        val multiPolygonStats = ZonalStats.toList
           val thePolygonKeys = polygons.keys.toList*/
-          // ZonalStats.toList.foreach(writer.write)
+            // ZonalStats.toList.foreach(writer.write)
 
-        } //vector
-      //tile
+          } //vector
+        } // run
+      }//tile
     }//raster
+  }
 
 
 
